ce7a78f2babd9fd96caa7cebc1a4246a157e8a28
radeya <hello.radeya@gmail.com>
2026-06-09 19:43:09 +0700

    Add Meta component for SEO and page metadata

---
+++ src/templates/couple/Meta.tsx
@@ -0,0 +1,99 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
+
+import { AppConfig } from './AppConfig';
+
+type IMetaProps = {
+  title: string;
+  description: string;
+  canonical?: string;
+};
+
+const Meta = (props: IMetaProps) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" key="charset" />
+
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+          key="viewport"
+        />
+
+        <link
+          rel="apple-touch-icon"
+          href={`${router.basePath}/apple-touch-icon.png`}
+          key="apple"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${router.basePath}/favicon-32x32.png`}
+          key="icon32"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${router.basePath}/favicon-16x16.png`}
+          key="icon16"
+        />
+
+        <link
+          rel="icon"
+          href={`${router.basePath}/favicon.ico`}
+          key="favicon"
+        />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;
+              n.push=n;
+              n.loaded=!0;
+              n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e);
+              t.async=!0;
+              t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}
+              (window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              fbq('init', '804715912719122');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* End Meta Pixel Code */}
+      </Head>
+
+      <NextSeo
+        title={props.title}
+        description={props.description}
+        canonical={props.canonical}
+        openGraph={{
+          title: props.title,
+          description: props.description,
+          url: props.canonical,
+          locale: AppConfig.locale,
+          site_name: AppConfig.site_name,
+        }}
+      />
+    </>
+  );
+};
+
+export { Meta };
