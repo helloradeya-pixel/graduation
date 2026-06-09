7c564a93082dca76902ad95407ce05e460d523ae
radeya <hello.radeya@gmail.com>
2026-06-09 20:46:44 +0700

    Add AppConfig for Radeyaphoto site configuration

---
+++ src/templates/couple/AppConfig.ts
@@ -0,0 +1,7 @@
+export const AppConfig = {
+  site_name: 'Radeyaphoto',
+  title: 'Radeyaphoto',
+  description:
+    'Radeyaphoto - Graduation & Wedding Photography. Capturing beautiful and timeless memories with an elegant touch.',
+  locale: 'en',
+};
