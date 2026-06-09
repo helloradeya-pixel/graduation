9b88032b97f3cced56629c0a129ca6ec8728d286
radeya <hello.radeya@gmail.com>
2026-06-09 21:03:10 +0700

    Refactor Button component to use classNames

--- src/templates/couple/Button.tsx
+++ src/templates/couple/Button.tsx
@@ -1,46 +1,39 @@
-import className from 'classnames';
+import classNames from 'classnames';
 
 type IButtonProps = {
   xl?: boolean;
-  children: string;
+  children: React.ReactNode;
 };
 
 const Button = (props: IButtonProps) => {
-  const btnClass = className({
-    btn: true,
-    'btn-xl': props.xl,
-    'btn-base': !props.xl,
-    'btn-primary': true,
-  });
+  const btnClass = classNames(
+    'btn',
+    props.xl ? 'btn-xl' : 'btn-base',
+    'btn-primary'
+  );
 
   return (
-    <div className={btnClass}>
+    <button className={btnClass}>
       {props.children}
 
-      <style jsx>
-        {`
-          .btn {
-            @apply inline-block rounded-md text-center;
-          }
+      <style jsx>{`
+        .btn {
+          @apply inline-block rounded-md text-center;
+        }
 
-          .btn-base {
-            @apply text-lg font-semibold py-2 px-4;
-          }
+        .btn-base {
+          @apply text-lg font-semibold py-2 px-4;
+        }
 
-          .btn-xl {
-            @apply font-extrabold text-xl py-2 px-4;
-          }
+        .btn-xl {
+          @apply font-extrabold text-xl py-2 px-4;
+        }
 
-          .btn-primary {
-            @apply text-white bg-[#C8A96A] hover:bg-[#b89655] text-white;
-          }
-
-          .btn-primary:hover {
-            @apply bg-primary-600;
-          }
-        `}
-      </style>
-    </div>
+        .btn-primary {
+          @apply text-white bg-[#C8A96A] hover:bg-[#b89655];
+        }
+      `}</style>
+    </button>
   );
 };
 
