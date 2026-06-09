2b70bd9577b0500b899fd12bd96319d9300cae70
radeya <hello.radeya@gmail.com>
2026-06-09 17:45:55 +0700

    Reformat paragraph for better readability

--- src/templates/Home/home.tsx
+++ src/templates/Home/home.tsx
@@ -15,8 +15,15 @@ export default function RadeyaphotoPage() {
         />
 
         <h1>Radeyaphoto</h1>
-        <p>Some memories live in soft light and silent smiles.
-I capture them so years from now, you’ll remember not just how it looked, but how it felt.</p>
+        <p>
+  Some memories live in soft light and silent smiles.
+  <br /><br />
+  I capture them so years from now,
+  <br />
+  you’ll remember not just how it looked,
+  <br />
+  but how it felt.
+</p>
 
         <div className="buttons">
 
