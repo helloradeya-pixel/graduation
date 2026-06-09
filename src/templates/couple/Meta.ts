443eba4dc375c2153046e0afbc49a015aabd95c0
radeya <hello.radeya@gmail.com>
2026-06-09 20:20:52 +0700

    Update Meta.ts

--- src/templates/couple/Meta.ts
+++ src/templates/couple/Meta.ts
@@ -1,58 +1,22 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
-
 import { AppConfig } from './AppConfig';
 
-type IMetaProps = {
+type MetaProps = {
   title: string;
   description: string;
   canonical?: string;
 };
 
-const Meta = (props: IMetaProps) => {
-  const router = useRouter();
-
+const Meta = ({ title, description, canonical }: MetaProps) => {
   return (
     <>
       <Head>
-        <meta charSet="UTF-8" key="charset" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
 
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1"
-          key="viewport"
-        />
+        <link rel="icon" href="/favicon.ico" />
 
-        <link
-          rel="apple-touch-icon"
-          href={`${router.basePath}/apple-touch-icon.png`}
-          key="apple"
-        />
-
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`${router.basePath}/favicon-32x32.png`}
-          key="icon32"
-        />
-
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`${router.basePath}/favicon-16x16.png`}
-          key="icon16"
-        />
-
-        <link
-          rel="icon"
-          href={`${router.basePath}/favicon.ico`}
-          key="favicon"
-        />
-
-        {/* Meta Pixel Code */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -77,17 +41,16 @@ const Meta = (props: IMetaProps) => {
             `,
           }}
         />
-        {/* End Meta Pixel Code */}
       </Head>
 
       <NextSeo
-        title={props.title}
-        description={props.description}
-        canonical={props.canonical}
+        title={title}
+        description={description}
+        canonical={canonical}
         openGraph={{
-          title: props.title,
-          description: props.description,
-          url: props.canonical,
+          title,
+          description,
+          url: canonical,
           locale: AppConfig.locale,
           site_name: AppConfig.site_name,
         }}
