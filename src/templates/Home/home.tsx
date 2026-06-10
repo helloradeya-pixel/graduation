d0c7b143a52a87c0948d545744359e28809a270a
radeya <hello.radeya@gmail.com>
2026-06-10 13:06:44 +0700

    Add event tracking for WhatsApp and Instagram links

--- src/templates/Home/home.tsx
+++ src/templates/Home/home.tsx
@@ -1,13 +1,52 @@
 import "./Radeyaphoto.css";
 import { Testimonial } from "../Testimonial";
 
+declare const fbq: any;
+
 export default function RadeyaphotoPage() {
+
+  const fireWAEvent = () => {
+    if (typeof window !== 'undefined') {
+      if ((window as any).fbq) {
+        (window as any).fbq('track', 'Contact', {
+          content_name: 'Bio Page WhatsApp',
+        });
+      }
+
+      if ((window as any).gtag) {
+        (window as any).gtag('event', 'click_whatsapp', {
+          event_category: 'bio_page',
+          event_label: 'wa_admin',
+        });
+      }
+    }
+  };
+
+  const fireIGWedding = () => {
+    if ((window as any).gtag) {
+      (window as any).gtag('event', 'click_instagram', {
+        event_category: 'outbound',
+        event_label: 'ig_wedding',
+      });
+    }
+  };
+
+  const fireIGWisuda = () => {
+    if ((window as any).gtag) {
+      (window as any).gtag('event', 'click_instagram', {
+        event_category: 'outbound',
+        event_label: 'ig_wisuda',
+      });
+    }
+  };
+
   return (
     <div className="radeyaphoto">
 
       <div className="hero"></div>
 
       <div className="profile-section">
+
         <img
           src="/assets/images/profile.jpg"
           alt="profile"
@@ -28,25 +67,31 @@ export default function RadeyaphotoPage() {
 
         <div className="buttons">
 
+          {/* WA ADMIN */}
           <a
             href="https://wa.me/628211251570"
             target="_blank"
+            onClick={fireWAEvent}
             className="link-btn"
           >
             WHATSAPP ADMIN
           </a>
 
+          {/* IG WEDDING */}
           <a
             href="https://instagram.com/radeyaphoto"
             target="_blank"
+            onClick={fireIGWedding}
             className="link-btn"
           >
             INSTAGRAM WEDDING
           </a>
 
+          {/* IG WISUDA */}
           <a
             href="https://instagram.com/radeya.graduation"
             target="_blank"
+            onClick={fireIGWisuda}
             className="link-btn"
           >
             INSTAGRAM WISUDA
@@ -57,11 +102,9 @@ export default function RadeyaphotoPage() {
 
       <Testimonial />
 
-      {/* BOTTOM */}
-        <div className="mt-12 border-t border-white/5 pt-6 text-center text-xs text-neutral-500">
-          © {new Date().getFullYear()} Radeya Photography. All rights reserved.
-        </div>
-
+      <div className="mt-12 border-t border-white/5 pt-6 text-center text-xs text-neutral-500">
+        © {new Date().getFullYear()} Radeya Photography. All rights reserved.
+      </div>
 
     </div>
   );
