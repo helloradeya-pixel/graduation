410a51308af8c2cd57e00d99e4de3008d9d8f7f0
radeya <hello.radeya@gmail.com>
2026-06-12 02:28:47 +0700

    Update couple component for tracking page views
    Refactor tracking code for couple page to use trackCoupleView function.

--- src/templates/couple/couple.tsx
+++ src/templates/couple/couple.tsx
@@ -2,30 +2,19 @@ import { useEffect } from 'react';
 
 import { Meta } from './Meta';
 import { AppConfig } from '../../utils/AppConfig';
-import { Banner } from './Banner';
-import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { Gallery } from './Gallery';
 import { Testimonial } from './Testimonial';
 import { LeadForm } from './LeadForm';
+import { Banner } from './Banner';
+import { Footer } from './Footer';
+
+import { trackCoupleView } from '@/utils/tracking';
 
 const Couple = () => {
-  // PAGE TRACKING
+  // TRACK PAGE VIEW (META + GA4)
   useEffect(() => {
-    // Google Analytics 4
-    if (typeof window !== 'undefined' && (window as any).gtag) {
-      (window as any).gtag('event', 'page_view_couple', {
-        page: 'couple',
-        source: 'landing_page',
-      });
-    }
-
-    // Meta Pixel
-    if (typeof window !== 'undefined' && (window as any).fbq) {
-      (window as any).fbq('trackCustom', 'ViewCouplePage', {
-        page: 'couple',
-      });
-    }
+    trackCoupleView();
   }, []);
 
   return (
