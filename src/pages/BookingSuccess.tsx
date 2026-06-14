0448ed6510d1cce3403373c4568765d963736684
radeya <hello.radeya@gmail.com>
2026-06-14 23:26:40 +0700

    Update and rename BookingSuccess.tsx to bookingsuccess.tsx

--- src/pages/BookingSuccess.tsx
+++
@@ -1,7 +0,0 @@
-// src/pages/BookingSuccessPage.tsx
-
-import BookingSuccess from '../templates/booking/bookingSuccess';
-
-const BookingSuccessPage = () => <BookingSuccess />;
-
-export default BookingSuccessPage;
