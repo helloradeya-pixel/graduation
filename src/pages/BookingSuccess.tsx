8b6edd51dd30b1c541632da575ae28963bb75e26
radeya <hello.radeya@gmail.com>
2026-06-14 23:15:30 +0700

    Create BookingSuccessPage component

---
+++ src/pages/BookingSuccess.tsx
@@ -0,0 +1,7 @@
+// src/pages/BookingSuccessPage.tsx
+
+import BookingSuccess from '../templates/booking/BookingSuccess';
+
+const BookingSuccessPage = () => <BookingSuccess />;
+
+export default BookingSuccessPage;
