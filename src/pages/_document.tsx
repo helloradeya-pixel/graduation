c6d5fa246dff10571007954360589de039667046
radeya <hello.radeya@gmail.com>
2026-07-12 19:03:07 +0700

    Remove Facebook Pixel and keep Google Analytics
    Removed Facebook Pixel script and retained Google Analytics script.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,57 +6,25 @@ class MyDocument extends Document {
     return (
       <Html lang={AppConfig.locale}>
         <Head>
-
-          {/* =========================
-              META PIXEL
-          ========================= */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;
-                n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);
-                t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}
-                (window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-
-                fbq('init', '804715912719122');
-                fbq('track', 'PageView');
-              `,
-            }}
-          />
-
-          {/* =========================
-              GOOGLE ANALYTICS (GA4)
-          ========================= */}
+          {/* GOOGLE ANALYTICS (GA4) - Dipertahankan di sini karena bersifat global */}
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-JLNW7X43JF"
           />
-
           <script
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 window.gtag = gtag;
-
                 gtag('js', new Date());
-
                 gtag('config', 'G-JLNW7X43JF', {
                   page_path: window.location.pathname,
                 });
               `,
             }}
           />
-
         </Head>
-
         <body>
           <Main />
           <NextScript />
