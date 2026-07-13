410425514ee356b24c85902a0dc62fe4e582df70
radeya <hello.radeya@gmail.com>
2026-07-13 16:52:14 +0700

    Enhance CouplePage with SEO metadata
    Added Head component for SEO enhancements with title and meta description.

--- src/pages/couple.tsx
+++ src/pages/couple.tsx
@@ -1,5 +1,16 @@
+import Head from 'next/head';
 import { Couple } from '../templates/couple/couple';
 
-const CouplePage = () => <Couple />;
+const CouplePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Fotografi Couple | Radeyaphoto</title>
+        <meta name="description" content="Jasa fotografi couple profesional dengan sentuhan estetik dan timeless oleh Radeyaphoto." />
+      </Head>
+      <Couple />
+    </>
+  );
+};
 
 export default CouplePage;
