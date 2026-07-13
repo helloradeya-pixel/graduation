ed99455629f951a6fd92854e21d039d31a125fa4
radeya <hello.radeya@gmail.com>
2026-07-13 16:43:02 +0700

    Enhance Links page with SEO metadata
    Add Head component for SEO and site title.

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head';
+import { AppConfig } from '../utils/AppConfig';
 import RadeyaphotoPage from "../templates/Home/home";
 
 export default function Links() {
-  return <RadeyaphotoPage />;
+  return (
+    <>
+      <Head>
+        <title>Link Bio | {AppConfig.site_name}</title>
+        <meta name="description" content={`Hubungi kami untuk layanan fotografi di ${AppConfig.site_name}.`} />
+      </Head>
+      <RadeyaphotoPage />
+    </>
+  );
 }
