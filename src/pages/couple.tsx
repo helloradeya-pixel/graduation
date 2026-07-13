24c9aa819cd65d55b3565f7473155523e3ceab4d
radeya <hello.radeya@gmail.com>
2026-07-13 16:57:33 +0700

    Simplify CouplePage by removing Head component
    Removed Head component and its metadata from CouplePage.

--- src/pages/couple.tsx
+++ src/pages/couple.tsx
@@ -1,16 +1,7 @@
-import Head from 'next/head';
 import { Couple } from '../templates/couple/couple';
 
 const CouplePage = () => {
-  return (
-    <>
-      <Head>
-        <title>Fotografi Couple | Radeyaphoto</title>
-        <meta name="description" content="Jasa fotografi couple profesional dengan sentuhan estetik dan timeless oleh Radeyaphoto." />
-      </Head>
-      <Couple />
-    </>
-  );
+  return <Couple />;
 };
 
 export default CouplePage;
